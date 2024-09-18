efa07c9378754edc1120ec2ba9f873179eddf54a
Iris <batista.iris@alunoescolassociais.org.br>
2024-09-18 08:34:05 -0300

    Criando uma função para fazer uma busca de email

--- models/user.php
+++ models/user.php
@@ -3,5 +3,19 @@
 //Inclui o arquivo de conexão que contem a class Database para gerenciar a conexão com o BD
 require_once 'models/database.php';
 
+class User
+{
+    //Função para encontrar um usuario pelo email
+    public static function findByEmail($email){
+        $conn = Database::getConnection();
 
+        $stmt = $conn ->prepare("SELECT * FROM usuarios  WHERE email = :email");
+        //executa a consulta com email passado como parametro
+        $stmt->execute(['email' => $email]);
+
+        //retorna os dados do usuario encontrado como um array associativo 
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+        
+    }
+}
 ?>
